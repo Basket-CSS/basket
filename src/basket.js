666968b7e84ac044ab98ee5f2194f7dd3b0bdb2e
Omar Quazi <omar@quazi.co>
2021-02-06 20:46:41 -0500

    added darkmode toggle and made navbar text uniform in dark and light mode

--- src/basket.js
+++ src/basket.js
@@ -1,7 +1,18 @@
-document.querySelectorAll('.navbar-collapse').forEach( el => {
+document.querySelectorAll('.navbar-collapse').forEach(el => {
     el.addEventListener('click', (ev) => {
         let targetElement = ev.target || ev.srcElement;
         targetElement = targetElement.parentElement.querySelector('.navbar-items');
         targetElement.classList.toggle('collapsed');
     });
+});
+
+document.querySelectorAll(".darkmode-toggler").forEach(el => {
+    el.onchange = () => {
+        if (el.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+        }
+    }
+
 })
